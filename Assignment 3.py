da5fc44ac57ea173835e9ec7d1815c0624f64094
ar3ifx <xdsept16@gmail.com>
2026-01-19 19:52:59 +0530

    Implement Task_2 for additional mathematical functions
    Added Task_2 to calculate square root, logarithm, and sine.

--- Assignment 3.py
+++ Assignment 3.py
@@ -1,3 +1,5 @@
+#Task_1
+
 def factorial(number):
     num=1
     while number>=1:
@@ -8,3 +10,12 @@ result=int(input("Enter a number: "))
 print(f"Factorial of {result} is:  {factorial(result)} ")
 
 
+#Task_2
+choice=float(input("Enter a number: "))
+import math
+squareroot=math.sqrt(choice)
+log=math.log(choice)
+sine=math.sin(choice)
+print(f"Square root: {squareroot} ")
+print(f"logarithm: {log} ")
+print(f"sine: {sine} ")
